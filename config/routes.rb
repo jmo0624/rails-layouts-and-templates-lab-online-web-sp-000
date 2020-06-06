2a1f765bb11a81212f50506330d7f4de419d5b04
Josh Morris <jmo0624@gmail.com>
2020-06-06 18:49:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 
+  get 'store/Admin'
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
